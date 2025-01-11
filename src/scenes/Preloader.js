fc6221d5eadd861d28f53ccca5c4743d7e7ff544
l7ana <liana@saedesigngroup.com>
2025-01-10 16:53:29 -1000

    forces webfont to load

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -61,9 +61,9 @@ export class Preloader extends Scene
     isMobile() {
         const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
         const deviceWidthSmall = screen.availHeight > screen.availWidth || window.innerHeight > window.innerWidth;
-        const isTouchDevice = this.sys.game.device.input.touch;
+        const isTouchDevice = Phaser.Input.Touch;
     
-        return regex.test(navigator.userAgent) || deviceWidthSmall ? true : false;
+        return regex.test(navigator.userAgent) || deviceWidthSmall || isTouchDevice ? true : false;
     }
     calculateLayout() {
         const gameWidth = this.cameras.main.width;
@@ -83,10 +83,18 @@ export class Preloader extends Scene
 
     preload ()
     {
+        const isTouchDevice = this.isMobile();
         //  Load the assets for the game - Replace with your own assets
         this.load.setPath('assets');
 
-        this.load.font('Price Check', 'pricecheck-webfont.woff', 'otf');
+        this.load.image('one', isTouchDevice ? 'scenes/01_Window_382x382_@2x.jpg' : 'assets/scenes/01_Window_890x593_@2x.jpg');
+        this.load.image('two', isTouchDevice ? 'scenes/02_Jars_382x283_@2x.jpg' : 'assets/scenes/02_Jars_890x593_@2x.jpg');
+        this.load.image('three', isTouchDevice ? 'scenes/03_Wallhooks_382x282_@2x.jpg' : 'assets/scenes/03_Wallhooks_890x593_@2x.jpg');
+        this.load.image('four', isTouchDevice ? 'scenes/04_OnFloor_382x382_@2x.jpg' : 'assets/scenes/04_OnFloor_890x593_@2x.jpg');
+        this.load.image('five', isTouchDevice ? 'scenes/05_EndScreen_382x382_@2x.jpg' : 'assets/scenes/05_EndScreen_890x593_@2x.jpg');
+
+        this.load.font('Price Check', 'pricecheck-webfont.woff', 'woff');
+        this.load.font('Price Check', 'pricecheck-webfont.woff', 'woff2');
 
         this.load.spritesheet('snake1','./sprites/SDG_snake_1.png', {frameWidth: 64, frameHeight: 64})
         this.load.spritesheet('sHead', './sprites/snakehead-spritesheet-01.png', {frameWidth: 64, frameHeight: 64})
